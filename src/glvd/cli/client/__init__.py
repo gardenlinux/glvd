536e9f4812b87dfe3cdf0c847cf8a7fc96701fb3
Bastian Blank <bastian.blank@netapp.com>
2023-12-15 13:06:29 +0100

    Add client tool to get single CVE

---
+++ src/glvd/cli/client/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from ..registry import CliRegistry
+
+
+cli = CliRegistry()
---
+++ src/glvd/cli/client/cve.py
@@ -0,0 +1,63 @@
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import json
+import logging
+import sys
+import urllib.parse
+
+from ...util import requests
+from . import cli
+
+
+logger = logging.getLogger(__name__)
+
+
+class ClientCve:
+    server: str
+
+    @staticmethod
+    @cli.register(
+        'cve',
+        arguments=[
+            cli.add_argument(
+                'cve',
+                help='the CVE to look up',
+                metavar='CVE',
+            ),
+            cli.add_argument(
+                '--server',
+                default='http://localhost:5000',
+                help='the server to use',
+            ),
+            cli.add_argument(
+                '--debug',
+                action='store_true',
+                help='enable debug output',
+            ),
+        ]
+    )
+    def run(cve: str, server: str, debug: bool) -> None:
+        logging.basicConfig(level=debug and logging.DEBUG or logging.INFO)
+        ClientCve(server)(cve)
+
+    def __init__(self, server: str) -> None:
+        self.server = server
+
+    def __call__(self, cve: str) -> None:
+        with requests.RetrySession() as rsession:
+            resp = rsession.get(
+                urllib.parse.urljoin(self.server, f'v1/cves/{cve}'),
+            )
+            if resp.status_code == 200:
+                data = resp.json()
+                json.dump(data, sys.stdout, indent=2)
+            elif resp.status_code == 404:
+                print(f'{cve} not found', file=sys.stderr)
+            else:
+                resp.raise_for_status()
+
+
+if __name__ == '__main__':
+    ClientCve.run()
