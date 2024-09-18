1901095926554f491e7369adfa0833d7f456569d
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-18 15:14:51 +0200

    Bootstrap k8s deployment

---
+++ deploy-k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DB_PASSWORD=$(pwgen 42 1)
+
+kubectl create secret generic postgres-credentials --type=string --from-literal=username=glvd --from-literal=password="$DB_PASSWORD"
+
+kubectl apply -f 00_db-statefulset.yaml
+
+sleep 20
+
+kubectl run init-pg --image=ghcr.io/gardenlinux/glvd-postgres-init:latest --restart=Never --env=PGHOST=glvd-database-0.glvd-database --env=PGPASSWORD="$DB_PASSWORD"
+
+sleep 60
+
+kubectl apply -f 01_glvd-deployment.yaml
