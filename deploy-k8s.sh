b589dc8ce76fc46c6852974e387dc06820dbd664
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-30 12:38:30 +0200

    Add optional pg admin for debug/admin work

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -9,6 +9,15 @@ else
     echo 'did not find existing db credentials, creating new'
 fi
 
+if kubectl get secret | grep -q pgadmin-credentials ; then
+    PGADMIN_PASSWORD=$(kubectl get secret/pgadmin-credentials --template="{{.data.password}}" | base64 -d)
+    echo 'found existing pgadmin credentials, re-using'
+else
+    PGADMIN_PASSWORD=$(pwgen 42 1)
+    kubectl create secret generic pgadmin-credentials --type=string --from-literal=password="$PGADMIN_PASSWORD"
+    echo 'did not find existing pgadmin credentials, creating new'
+fi
+
 kubectl apply -f 00_db-statefulset.yaml
 
 echo 'give db some time to pull image and start'
