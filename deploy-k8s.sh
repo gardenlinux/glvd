a0fe7f5ec2ea5be12113ba6e72917b5b51f29fe1
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-11-06 09:16:02 +0100

    Configure glvd namespace

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -10,36 +10,36 @@ if ! [ -x "$(command -v kubectl)" ]; then
   exit 1
 fi
 
-if kubectl get secret | grep -q postgres-credentials ; then
-    DB_PASSWORD=$(kubectl get secret/postgres-credentials --template="{{.data.password}}" | base64 -d)
+if kubectl --namespace glvd get secret | grep -q postgres-credentials ; then
+    DB_PASSWORD=$(kubectl --namespace glvd get secret/postgres-credentials --template="{{.data.password}}" | base64 -d)
     echo 'found existing db credentials, re-using'
 else
     DB_PASSWORD=$(pwgen 42 1)
-    kubectl create secret generic postgres-credentials --type=string --from-literal=username=glvd --from-literal=password="$DB_PASSWORD"
+    kubectl --namespace glvd create secret generic postgres-credentials --type=string --from-literal=username=glvd --from-literal=password="$DB_PASSWORD"
     echo 'did not find existing db credentials, creating new'
 fi
 
-if kubectl get secret | grep -q pgadmin-credentials ; then
-    PGADMIN_PASSWORD=$(kubectl get secret/pgadmin-credentials --template="{{.data.password}}" | base64 -d)
+if kubectl --namespace glvd get secret | grep -q pgadmin-credentials ; then
+    PGADMIN_PASSWORD=$(kubectl --namespace glvd get secret/pgadmin-credentials --template="{{.data.password}}" | base64 -d)
     echo 'found existing pgadmin credentials, re-using'
 else
     PGADMIN_PASSWORD=$(pwgen 42 1)
-    kubectl create secret generic pgadmin-credentials --type=string --from-literal=password="$PGADMIN_PASSWORD"
+    kubectl --namespace glvd create secret generic pgadmin-credentials --type=string --from-literal=password="$PGADMIN_PASSWORD"
     echo 'did not find existing pgadmin credentials, creating new'
 fi
 
-kubectl apply -f deployment/k8s/00_db-statefulset.yaml
+kubectl --namespace glvd apply -f deployment/k8s/00_db-statefulset.yaml
 
 echo 'give db some time to pull image and start'
 sleep 30
 
-if kubectl get po | grep -q init-pg ; then
+if kubectl --namespace glvd get po | grep -q init-pg ; then
     echo 'init-pg container exists, skipping init for now'
 else
-    kubectl run init-pg --image=ghcr.io/gardenlinux/glvd-init:latest --restart=Never --env=PGHOST=glvd-database-0.glvd-database --env=PGPASSWORD="$DB_PASSWORD"
+    kubectl --namespace glvd run init-pg --image=ghcr.io/gardenlinux/glvd-init:latest --restart=Never --env=PGHOST=glvd-database-0.glvd-database --env=PGPASSWORD="$DB_PASSWORD"
     echo 'give init some time to complete'
     sleep 60
 fi
 
-kubectl apply -f deployment/k8s/01_glvd-deployment.yaml
-kubectl apply -f deployment/k8s/02_ingestion-job.yaml
+kubectl --namespace glvd apply -f deployment/k8s/01_glvd-deployment.yaml
+kubectl --namespace glvd apply -f deployment/k8s/02_ingestion-job.yaml
